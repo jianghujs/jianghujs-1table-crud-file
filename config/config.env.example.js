fb3a44a366e73fe56066c40f89219fa8c7ac46fa
colin <colin>
2024-04-16 20:45:36 +0800

    config

--- config/config.env.example.js
+++ config/config.env.example.js
@@ -6,8 +6,6 @@ require('dotenv').config({path: path.resolve(__dirname, '../../.env')});
 module.exports = appInfo => {
 
   return {
-    appType: 'multiApp',
-    appDirectoryLink: '/directory',
     static: {
       maxAge: 0,
       buffer: false,
